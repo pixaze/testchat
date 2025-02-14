eb4d72665058632b310d194e033d730d2e424e28
pixaze <serperkanada7@gmail.com>
2025-02-14 13:19:06 +0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -63,24 +63,3 @@ async function verifyUser(userId) {
     alert("User berhasil diverifikasi!");
 }
 
-function loadUsers() {
-    const userList = document.getElementById("user-list");
-    userList.innerHTML = "";
-
-    onSnapshot(collection(db, "users"), (snapshot) => {
-        snapshot.forEach((doc) => {
-            const user = doc.data();
-            if (user.email !== auth.currentUser.email) {
-                const div = document.createElement("div");
-                div.classList.add("user");
-                div.innerHTML = `
-                    <img src="${user.avatar}" width="40">
-                    <span>${user.username} ${user.verified ? "✔️" : ""}</span>
-                    <span>${user.online ? "🟢 Online" : "⚪ Offline"}</span>
-                `;
-                div.onclick = () => startChat(doc.id, user.username);
-                userList.appendChild(div);
-            }
-        });
-    });
-}
