0f7b48c4bc3ce1266b1e224fc65aecd8d4dd4030
pixaze <serperkanada7@gmail.com>
2025-03-08 19:09:47 +0700

    Update feed.js

--- feed.js
+++ feed.js
@@ -116,7 +116,15 @@ async function displayPosts(posts) {
                     <div class="user-details">
                         <div class="username-container">
                             <span class="username">${postData.userName}</span>
-                            ${postData.verifiedvip ? `<span class="verified-badge"><svg id="verifiedvip" width="16" height="16" viewBox="0 0 40 40"><defs><linearGradient id="gold-gradient" x1="4" y1="2" x2="36" y2="38"><stop offset="0%" stop-color="#f4e72a"/><stop offset="50%" stop-color="#cd8105"/><stop offset="100%" stop-color="#f4e72a"/></linearGradient></defs><path d="M19.998 3.094 14.638 0l-2.972 5.15H5.432v6.354L0 14.64 3.094 20 0 25.359l5.432 3.137v5.905h5.975L14.638 40l5.36-3.094L25.358 40l3.232-5.6h6.162v-6.01L40 25.359 36.905 20 40 14.641l-5.248-3.03v-6.46h-6.419L25.358 0l-5.36 3.094Z" fill="url(#gold-gradient)"/><path d="M27.413 14.319l2.254 2.287-11.43 11.5-6.835-6.93 2.244-2.258 4.587 4.581 9.18-9.18Z" fill="black"/></svg></span>` : 
+                            ${postData.verifiedvip ? `<span class="verified-badge"><svg id="verifiedvip" width="40" height="40" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
+                            <defs>
+        <linearGradient id="gold-gradient" x1="4" y1="2" x2="36" y2="38" gradientUnits="userSpaceOnUse">
+            <stop offset="0%" stop-color="#f4e72a"/>
+            <stop offset="50%" stop-color="#cd8105"/>
+            <stop offset="100%" stop-color="#f4e72a"/>
+        </linearGradient>
+    </defs>
+    <path d="M19.998 3.094 14.638 0l-2.972 5.15H5.432v6.354L0 14.64 3.094 20 0 25.359l5.432 3.137v5.905h5.975L14.638 40l5.36-3.094L25.358 40l3.232-5.6h6.162v-6.01L40 25.359 36.905 20 40 14.641l-5.248-3.03v-6.46h-6.419L25.358 0l-5.36 3.094Z" fill="url(#gold-gradient)"/><path d="M27.413 14.319l2.254 2.287-11.43 11.5-6.835-6.93 2.244-2.258 4.587 4.581 9.18-9.18Z" fill="black"/></svg></span>` : 
                             postData.verified ? `<span class="verified-badge"><svg id="verified" width="16" height="16" viewBox="0 0 40 40"><path d="M19.998 3.094 14.638 0l-2.972 5.15H5.432v6.354L0 14.64 3.094 20 0 25.359l5.432 3.137v5.905h5.975L14.638 40l5.36-3.094L25.358 40l3.232-5.6h6.162v-6.01L40 25.359 36.905 20 40 14.641l-5.248-3.03v-6.46h-6.419L25.358 0l-5.36 3.094Z" fill="currentColor"/><path d="M27.413 14.319l2.254 2.287-11.43 11.5-6.835-6.93 2.244-2.258 4.587 4.581 9.18-9.18Z" fill="white"/></svg></span>` : ''}
                         </div>
                         <span class="timestamp">${formatTimestamp(postData.createdAt)}</span>
